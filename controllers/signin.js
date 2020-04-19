fbad7fff6238bfde2a13c30c03266a413801b6cf
Erin Crane <ecrane@Erins-MacBook-Air.local>
2020-04-19 09:20:02 -0400

    fix table name again

--- controllers/signin.js
+++ controllers/signin.js
@@ -3,7 +3,7 @@ const handleSignIn = (req, res, db, bcrypt) => {
     if (!email || !password) {
         return res.status(400).json('incorrect form submission');
     }
-    db.select('email','hash').from('logins')
+    db.select('email','hash').from('login')
         .where('email','=',email)
         .then(data => {
             const isValid = bcrypt.compareSync(password, data[0].hash);
